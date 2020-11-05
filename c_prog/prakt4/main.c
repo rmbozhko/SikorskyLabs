edf802d5e48b1eb59752defca91727af54fba94b
Roman Bozhko <rm.bozhko@gmail.com>
2020-11-05 20:24:59 +0200

    Added module to free the allocated memeory

--- c_prog/prakt4/main.c
+++ c_prog/prakt4/main.c
@@ -3,14 +3,14 @@
 
 #define SIZE 3
 
-void  ft_swap(int *a, int *b) {
+void  	ft_swap(int *a, int *b) {
   int   temp;
   temp = *a;
   *a = *b;
   *b = temp;
 }
 
-int   **initialize_matrix(void) {
+int   	**initialize_matrix(void) {
   int  **bi_iarr;
   int   temp;
 
@@ -27,22 +27,32 @@ int   **initialize_matrix(void) {
 }
 
 
-void  rotate_matrix(int **bi_iarr) {
+void  	rotate_matrix(int **bi_iarr) {
   for (int i = 0; i < SIZE; i++) {
     ft_swap(&bi_iarr[i][0], &bi_iarr[i][2]);
   }
-  printf("Matrix was rotated vertically through its center\n");
 }
 
-void  print_matrix(int **bi_iarr) {
+void  	print_matrix(int **bi_iarr) {
   for (int i = 0; i < SIZE; i++) {
     printf("|%d %d %d|\n", bi_iarr[i][0], bi_iarr[i][1], bi_iarr[i][2]);
   }
 }
 
-int   main(void) {
-  int   **bi_iarr = initialize_matrix();
+void	free_matrix(int	**bi_iarr) {
+  for (int i = 0; i < SIZE; i++) {
+    free(bi_iarr[i]);
+  }
+  free(bi_iarr);
+}
+
+int   	main(void) {
+  int   **bi_iarr;
+  
+  bi_iarr = initialize_matrix();
   print_matrix(bi_iarr);
   rotate_matrix(bi_iarr);
+  printf("\nMatrix was rotated vertically through its center\n\n");
   print_matrix(bi_iarr);
+  free_matrix(bi_iarr);
 }
