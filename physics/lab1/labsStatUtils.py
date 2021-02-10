486ab2997ad79ea7b5f5d6933eb13e1cbe377511
rmbozhko <rm.bozhko@gmail.com>
2021-02-10 12:40:34 +0200

    Fixed relative error calc due to @sodabottle comment

--- physics/lab1/labsStatUtils.py
+++ physics/lab1/labsStatUtils.py
@@ -2,7 +2,6 @@ from statistics import mean
 from decimal import Decimal,ROUND_HALF_UP
 from math import sqrt
 
-
 def getMean(data):
     ten_places = Decimal('0.0000000001')
     four_places = Decimal('0.0001')
@@ -21,12 +20,9 @@ def getMSE(arr):
     result = sqrt(result/(n * (n - 1)))
     return result
 
-# TO-DO: Check the main flow
 def getRelativeError(data):
     dataMean = mean(data)
     temp = data
-    absoleteError = sum(list(map(lambda dataPoint: dataPoint - dataMean, temp)))
-    temp = data
-    relativeError = absoleteError / dataMean
-    return relativeError
-    
+    absoleteErrors = list(map(lambda dataPoint: dataPoint - dataMean, temp))
+    relativeErrors = list(map(lambda dataPoint: abs(dataPoint / dataMean), absoleteErrors)) # is abs necessary?
+    return mean(relativeErrors)
