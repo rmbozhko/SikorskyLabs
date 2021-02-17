faaed4466a41862e073ea70e06e7d9d35626cb13
repl.it user <replituser@example.com>
2021-02-17 05:31:39 +0000

    Completed and tested first part of l7 project

--- c_prog/lab7/main.c
+++ c_prog/lab7/main.c
@@ -3,53 +3,56 @@
 
 #define SIZE 10
 
-int	find_odd(const int* arr) {
+int	find_odd(int** arr) {
 	for (int i = 0; i < SIZE; ++i) {
-		if (arr[i] % 2 != 0) {
+		if (*arr[i] % 2 != 0) {
 			return i;
 		}
 	}
 	return -1;
 }
 
-void shift_elements(int* arr, const int index) {
+void shift_elements(int** arr, const int index) {
 	if (index == SIZE - 1) {
 		free(arr[index]);
 		return ;
 	} else {
-		arr[index] = index[index + 1]
+		*arr[index] = *arr[index + 1];
 	}
 	shift_elements(arr, index + 1);
 }
 
-void pop(int* arr, const int index) {
+void pop(int** arr, const int index) {
 	if (index >= 0 && index < SIZE) {
 		shift_elements(arr, index);
 	}
 }
 
-void print_arr(int* arr, const int len) {
-	printf("{");
+void print_arr(int** arr, const int len) {
+	printf("{ ");
 	for (int i = 0; i < len; ++i) {
-		printf("%d ", arr[i]);
+		printf("%d ", *arr[i]);
 	}
 	printf("}\n");
 }
 
 int main(int argc, char const *argv[])
 {
-	int*		arr;
-
-	arr = (int*)malloc(int * SIZE);
+  
+	int**		arr;
+	arr = (int**)malloc(sizeof(int*) * SIZE);
+  
 	for (int i = 0; i < SIZE; ++i) {
-		arr[i] = rand() % 50;
+    arr[i] = (int*)malloc(sizeof(int));
+		*arr[i] = rand() % 50;
 	}
-
+  printf("Initial array: ");
 	print_arr(arr, SIZE);
 
 	const int index = find_odd(arr);
 	pop(arr, index);
-
+  
+  printf("Modified array: ");
 	if (index != -1) {
 		print_arr(arr, SIZE - 1);
 	} else {
