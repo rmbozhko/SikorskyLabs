5d8e8db049fa18fe7a41b06d6c63378798fd4161
rmbozhko <rm.bozhko@gmail.com>
2021-05-17 13:44:27 +0300

    Code for lecture #4
    Andrey Grehov: Dynamic Programming for Beginners

--- complex_algo/lab7/DynamicProgrammingPractice.java
+++ complex_algo/lab7/DynamicProgrammingPractice.java
@@ -18,7 +18,29 @@ public class DynamicProgrammingPractice {
 		return arr[n];
 	}
 
+	/*
+		1. Objective function: f(i) is a number of distinct ways to reach i-th stair.
+		2. Base cases: f(0) = 1, f(1) = 1, f(2) = 2
+		3. Recurrence relation; f(n) = f(n - 1) + f(n - 2)
+		4. Order of execution: Bottom-up
+	*/
+	static int 	climbingStairs(int n) {
+		int[] arr;
+
+		arr = new int[n + 1];
+		if (n < 0) return (-1);
+		if (n >= 0) arr[0] = 1;
+		if (n >= 1) arr[1] = 1;
+		
+		for (int i = 2; i <= n; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2];
+		}
+
+		return arr[n];	
+	}
+
 	public static void main(String[] args) {
+		/*
 		System.out.println("Sum for elements of 0: " + sum(0));
 		System.out.println("Sum for elements of 1: " + sum(1));
 		System.out.println("Sum for elements of 2: " + sum(2));
@@ -26,5 +48,15 @@ public class DynamicProgrammingPractice {
 		System.out.println("Sum for elements of 150: " + sum(150));
 		System.out.println("Sum for elements of 1500: " + sum(1500));
 		System.out.println("Sum for elements of 15000: " + sum(15000));
+		*/
+
+		System.out.println("Number of ways to get to stair #0: " + climbingStairs(0));
+		System.out.println("Number of ways to get to stair #1: " + climbingStairs(1));
+		System.out.println("Number of ways to get to stair #2: " + climbingStairs(2));
+		System.out.println("Number of ways to get to stair #3: " + climbingStairs(3));
+		System.out.println("Number of ways to get to stair #5: " + climbingStairs(5));
+		System.out.println("Number of ways to get to stair #150: " + climbingStairs(150));
+		System.out.println("Number of ways to get to stair #1500: " + climbingStairs(1500));
+		System.out.println("Number of ways to get to stair #15000: " + climbingStairs(15000));
 	}
 }
